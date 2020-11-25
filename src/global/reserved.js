08f5b1bad4f3d6412212d13bc15ce9c1897d7edb
Gibson Han <gibsonxhan@gmail.com>
2020-11-24 17:48:51 -0800

    add new reserve words

--- src/global/reserved.js
+++ src/global/reserved.js
@@ -1,1 +1,18 @@
-export const FULLSCREEN = 'fullscreen'
+export const FULLSCREEN = 'Fullscreen'
+export const EXPAND_FULLSCREEN = 'Expand Fullscreen'
+export const CLOSE_FUllSCREEN = 'Close Fullscreen'
+
+export const FILTER = 'Filter'
+export const RESET_FILTER = 'Reset Filter'
+export const SHOW_FILTER = 'Show Filter'
+export const HIDE_FILTER = 'Hide Filter'
+
+export const DEFAULT_CLASSNAME = 'default classname'
+export const DEFAULT_ICON = 'default icon'
+export const DEFAULT_TITLE = 'default title'
+export const DEFAULT_TEXT = 'default text'
+
+export const ACTIVE_CLASSNAME = 'active classname'
+export const ACTIVE_ICON = 'active icon'
+export const ACTIVE_TITLE = 'active title'
+export const ACTIVE_TEXT = 'active text'
