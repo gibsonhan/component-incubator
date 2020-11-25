71ba109474cbff753171a2855288a77d786b4a2b
Gibson Han <gibsonxhan@gmail.com>
2020-11-24 17:48:20 -0800

    refactor styling and button component to handle different type

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -3,43 +3,78 @@ import clsx from 'clsx'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
 
-import { FULLSCREEN } from '../../global/reserved';
+import {
+    ACTIVE_CLASSNAME,
+    ACTIVE_ICON,
+    ACTIVE_TEXT,
+    ACTIVE_TITLE,
+    DEFAULT_CLASSNAME,
+    DEFAULT_ICON,
+    DEFAULT_TEXT,
+    DEFAULT_TITLE,
+    CLOSE_FUllSCREEN,
+    EXPAND_FULLSCREEN,
+    FULLSCREEN,
+    HIDE_FILTER,
+    RESET_FILTER
+} from '../../global/reserved';
 import './Button.scss'
 
 const propTypes = {
     state: PropTypes.bool,
     type: PropTypes.string,
-    onClick: PropTypes.function,
+    onClick: PropTypes.func,
 }
 
-const renderFullScreenIcon = (state) => {
-    const selectIcon = state === false
-        ? <FontAwesomeIcon icon="expand-arrows-alt" aria-label="expand fullscreen" />
-        : <FontAwesomeIcon icon="times-circle" aria-label="exit fullscreen" />
-    return <i className="button__icon">{selectIcon}</i>
-}
+//Not sure if I should use array or hasttable again to present 
 
-function renderTitle(state) {
-    return state === false ? 'Press to Full Screen' : 'Press to Exit Full Screen'
-}
+
 
 export default function Button({ type, state, onClick }) {
-    const ICON = {
-        [FULLSCREEN]: renderFullScreenIcon(state)
+    const ICON_TABLE = {
+        [FULLSCREEN]: {
+            [DEFAULT_ICON]: <FontAwesomeIcon icon="expand-arrows-alt" aria-label="expand fullscreen" />,
+            [DEFAULT_CLASSNAME]: 'button__fullscreen--expand',
+            [DEFAULT_TEXT]: EXPAND_FULLSCREEN,
+            [DEFAULT_TITLE]: 'Press to Fullscreen',
+            [ACTIVE_ICON]: <FontAwesomeIcon icon="times-circle" aria-label="exit fullscreen" />,
+            [ACTIVE_CLASSNAME]: 'button__fullscreen--exit',
+            [ACTIVE_TEXT]: CLOSE_FUllSCREEN,
+            [ACTIVE_TITLE]: 'Presss to Exit Fullscreen'
+        },
     }
-    const TITLE = {
-        [FULLSCREEN]: renderTitle(state)
+
+    function renderIcon(key, state) {
+        return state === false
+            ? ICON_TABLE[key][DEFAULT_ICON]
+            : ICON_TABLE[key][ACTIVE_ICON]
     }
-    const BUTTONCLASSNAME = {
-        [FULLSCREEN]: clsx({ 'button__expandFullScreen': !state, 'button__exitFullScreen': state })
+
+    function renderTitle(key, state) {
+        return state === false
+            ? ICON_TABLE[key][DEFAULT_TITLE]
+            : ICON_TABLE[key][ACTIVE_TITLE]
+    }
+
+    function renderClassName(key, state) {
+        return state == false
+            ? ICON_TABLE[key][DEFAULT_CLASSNAME]
+            : ICON_TABLE[key][ACTIVE_CLASSNAME]
+    }
+
+    function renderButtonText(key, state) {
+        return state === false
+            ? ICON_TABLE[key][DEFAULT_TEXT]
+            : ICON_TABLE[key][ACTIVE_TEXT]
     }
 
     return (
-        <div className="button__container" onClick={onClick}>
-            <button className={BUTTONCLASSNAME[type]} title={TITLE[type]} type="button">
-                {ICON[type]}
-            </button>
-        </div >
+        <span className={renderClassName(type, state)}>
+            <div className="button__container" title={renderTitle(type, state)} onClick={onClick}>
+                <i className="button__icon">{renderIcon(type, state)}</i>
+                <div className="button__text">{renderButtonText(type, state)}</div>
+            </div >
+        </span >
     )
 }
 
