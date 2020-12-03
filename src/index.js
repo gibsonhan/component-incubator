db8165e145c4b0ab4ee8d34aa241721c170fe9c8
gibsonhan <gibsonxhan@gmail.com>
2020-12-03 14:29:46 -0800

    Revert "Feat filter"

--- src/index.js
+++ src/index.js
@@ -1,18 +1,12 @@
-import { faChevronLeft } from '@fortawesome/free-solid-svg-icons/faChevronLeft';
-import { faChevronRight } from '@fortawesome/free-solid-svg-icons/faChevronRight';
-import { faExpandArrowsAlt } from '@fortawesome/free-solid-svg-icons/faExpandArrowsAlt';
-import { faFilter } from '@fortawesome/free-solid-svg-icons/faFilter';
-import { faRedo } from '@fortawesome/free-solid-svg-icons/faRedo';
-import { faSort } from '@fortawesome/free-solid-svg-icons/faSort';
-import { faSortDown } from '@fortawesome/free-solid-svg-icons/faSortDown';
-import { faSortUp } from '@fortawesome/free-solid-svg-icons/faSortUp';
-import { faTimesCircle } from '@fortawesome/free-solid-svg-icons/faTimesCircle';
+import {faChevronLeft} from '@fortawesome/free-solid-svg-icons/faChevronLeft';
+import {faChevronRight} from '@fortawesome/free-solid-svg-icons/faChevronRight';
+import {faSort} from '@fortawesome/free-solid-svg-icons/faSort';
+import {faSortDown} from '@fortawesome/free-solid-svg-icons/faSortDown';
+import {faSortUp} from '@fortawesome/free-solid-svg-icons/faSortUp';
 
-import { library } from '@fortawesome/fontawesome-svg-core';
+import {library} from '@fortawesome/fontawesome-svg-core';
 
 library.add(
   faChevronLeft, faChevronRight,
   faSort, faSortDown, faSortUp,
-  faExpandArrowsAlt, faTimesCircle,
-  faFilter, faRedo,
 );
